1ff75b4bd15619067849ae51153a10c9447b5ff4
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-02 12:35:53 +0100

    Pagination component renders desired number of pages now - Added LODASH library (optimization of the underscore lib)

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from "lodash"; // LODASH is the optimized library of the popular "underscore" library
 
 // Decide on the interface of the component
 // What props does it take?
@@ -6,7 +7,25 @@ import React from "react";
 // The good way do make decision is to use the component before implementing it
 
 const Pagination = props => {
-  return null;
+  const { pageSize, itemsCount, onPageChange } = props;
+
+  const pageCount = itemsCount / pageSize;
+
+  // we use the lodash to generate an array thats going to look like
+  // [1, 2, ....., pageCount]
+  const pages = _.range(1, pageCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
