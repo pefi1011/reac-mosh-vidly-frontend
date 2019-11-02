7fc31805559c0d9b2592d13bf2d47a9606084a64
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-02 11:42:15 +0100

    Converted Like from CC to SFC

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 
-// Input: liked: boolean
-// Output: onClick (who ever is using this component to tell what to render)
-
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-
-    // onClick event in the i element is the DOM onClick element
-    // by this.props.onClick we are raising another custom event called "onClick"
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-      ></i>
-    );
-  }
-}
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+    ></i>
+  );
+};
 
 export default Like;
