45a4403e0cf442926d2f5fb1daba3956a12bef68
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-03 15:07:35 +0100

    Doing DOB of props of SFC directly in the parameters sections instead of within the function itself

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const Table = props => {
-  const { columns, sortColumn, onSort, data } = props;
+// BEFORE
+//const Table = props => {
+//  const { columns, sortColumn, onSort, data } = props;
 
+// NOW
+// // doing destructuring directly in the parameters in stead as first line of the SFC
+const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
     <table className="table">
       <TableHeader
