a2d612988a2195c1a613eedab7939b8df5fbadcd
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-03 14:32:01 +0100

    Added keys in the tableBody

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -25,18 +25,21 @@ class TableBody extends Component {
     return (
       <tbody>
         {data.map(item => (
-          <tr>
+          // we can make key more flexible by sending an additional prop which
+          // says what property should be used for the key
+          // and we could set the default value for this prop like we did it
+          // in section 11
+          <tr key={item._id}>
             {columns.map(column => (
-              // we use the bracket annotation to access a property dynamically
-              // however, this only works with simple properties
-              // <td className="">{item[column.path]}</td>
+              // the key like this will work only for the columns without the Like and Delete button
+              // because we do not have path property
+              // <td key={item._id + column.path} className="">{this.renderCell(item, column)}</td>
 
-              // if you are dealing with nested properties, this does not work
-              // our nested property is genre.name
-              // <td className="">{item["genre.name"]}</td>
-
-              // SO WE USE LODASH AGAIN
-              <td className="">{this.renderCell(item, column)}</td>
+              // we will use the logical OR operator
+              // if column.path is available, use it, otherwise use column.key
+              <td key={item._id + (column.path || column.key)} className="">
+                {this.renderCell(item, column)}
+              </td>
             ))}
           </tr>
         ))}
