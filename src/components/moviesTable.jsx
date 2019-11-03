5fc968317ebb8cffb2603fa01700e4bcdf359ded
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-03 12:25:59 +0100

    Refactored table movies to bootstrap table

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -4,46 +4,44 @@ import Like from "./common/like";
 import Pagination from "./common/pagination";
 
 const MoviesTable = props => {
-  const { movies, onDelete, onLike } = props;
+  const { movies, onDelete, onLike, onSort } = props;
 
   return (
     <React.Fragment>
       <hr></hr>
-      <div id="table-header" className="row">
-        <div className="col font-weight-bold">Test</div>
-        <div className="col font-weight-bold">Genre</div>
-        <div className="col font-weight-bold">Stock</div>
-        <div className="col font-weight-bold">Rate</div>
-        <div className="col font-weight-bold"></div>
-        <div className="col font-weight-bold"></div>
-      </div>
-      <hr></hr>
-      <div id="table-content">
-        {movies.map(movie => (
-          <div key={movie._id}>
-            <div className="row">
-              <div className="col">{movie.title}</div>
-              <div className="col">{movie.genre.name}</div>
-              <div className="col">{movie.numberInStock}</div>
-              <div className="col">{movie.dailyRentalRate}</div>
-              <div className="col">
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="col">Title</th>
+            <th className="col">Genre</th>
+            <th className="col">Stock</th>
+            <th className="col">Rate</th>
+            <th className="col"></th>
+            <th className="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map(movie => (
+            <tr key={movie._id}>
+              <th className="row"> {movie.title} </th>
+              <td className="">{movie.genre.name}</td>
+              <td className="">{movie.numberInStock}</td>
+              <td className="">{movie.dailyRentalRate}</td>
+              <td className="">
                 <Like onClick={() => onLike(movie)} liked={movie.liked}></Like>
-              </div>
-              <div className="col">
+              </td>
+              <td className="">
                 <button
                   className="btn btn-danger btn-sm"
                   onClick={() => onDelete(movie._id)}
                 >
                   Delete
                 </button>
-              </div>
-            </div>
-            <hr></hr>
-          </div>
-        ))}
-
-        <div className="row"></div>
-      </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </React.Fragment>
   );
 };
