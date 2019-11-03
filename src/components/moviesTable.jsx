3a1bfc5a8db1bf0d4c27ca7566fbc138ed796a88
Filip S. Perisic <filip.s.perisic@sap.com>
2019-11-03 13:32:56 +0100

    Promoted MovieTables component from SFC to CC

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,56 +1,63 @@
-import React from "react";
-
+import React, { Component } from "react";
 import Like from "./common/like";
 
-const MoviesTable = props => {
-  const { movies, onDelete, onLike, onSort } = props;
+// We converted the SFC to the CC because we added a method for determining the sort order
+// We promoted MovieTable component from SFC to CC
+class MoviesTable extends Component {
+  render() {
+    // we changed "props" to "this.props" because the props is not a parameter like in SFC
+    const { movies, onDelete, onLike, onSort } = this.props;
 
-  return (
-    <React.Fragment>
-      <hr></hr>
-      <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => onSort("title")} className="col">
-              Title
-            </th>
-            <th onClick={() => onSort("genre.name")} className="col">
-              Genre
-            </th>
-            <th onClick={() => onSort("numberInStock")} className="col">
-              Stock
-            </th>
-            <th onClick={() => onSort("dailyRentalRate")} className="col">
-              Rate
-            </th>
-            <th className="col"></th>
-            <th className="col"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <th className="row"> {movie.title} </th>
-              <td className="">{movie.genre.name}</td>
-              <td className="">{movie.numberInStock}</td>
-              <td className="">{movie.dailyRentalRate}</td>
-              <td className="">
-                <Like onClick={() => onLike(movie)} liked={movie.liked}></Like>
-              </td>
-              <td className="">
-                <button
-                  className="btn btn-danger btn-sm"
-                  onClick={() => onDelete(movie._id)}
-                >
-                  Delete
-                </button>
-              </td>
+    return (
+      <React.Fragment>
+        <hr></hr>
+        <table className="table">
+          <thead>
+            <tr>
+              <th onClick={() => onSort("title")} className="col">
+                Title
+              </th>
+              <th onClick={() => onSort("genre.name")} className="col">
+                Genre
+              </th>
+              <th onClick={() => onSort("numberInStock")} className="col">
+                Stock
+              </th>
+              <th onClick={() => onSort("dailyRentalRate")} className="col">
+                Rate
+              </th>
+              <th className="col"></th>
+              <th className="col"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </React.Fragment>
-  );
-};
+          </thead>
+          <tbody>
+            {movies.map(movie => (
+              <tr key={movie._id}>
+                <th className="row"> {movie.title} </th>
+                <td className="">{movie.genre.name}</td>
+                <td className="">{movie.numberInStock}</td>
+                <td className="">{movie.dailyRentalRate}</td>
+                <td className="">
+                  <Like
+                    onClick={() => onLike(movie)}
+                    liked={movie.liked}
+                  ></Like>
+                </td>
+                <td className="">
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDelete(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MoviesTable;
