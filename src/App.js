a0d808518bc4cdcfec2d8baf51ad6c370695ecb7
Filip S. Perisic <pefi1011@gmail.com>
2019-11-04 21:01:59 +0100

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFound from "./components/common/notfound";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
 
 function App() {
   return (
-    <main className="container">
-      <Movies></Movies>
-    </main>
+    <React.Fragment>
+      <NavBar></NavBar>
+      <main className="container">
+        <Switch>
+          <Route path="/home" exact component={Movies}></Route>
+          <Route path="/customers" exact component={Customers}></Route>
+          <Route path="/rentals" exact component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Route path="/" exact component={Movies}></Route>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+        <></>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <React.Fragment>
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-light"
+        style={{ marginBottom: "20px", marginTop: "0px" }}
+      >
+        <NavLink className="nav-link" to="/">
+          Vidly <span className="sr-only">(current)</span>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/">
+                Movies <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/customers">
+                Customers <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/rentals">
+                Rentals <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
