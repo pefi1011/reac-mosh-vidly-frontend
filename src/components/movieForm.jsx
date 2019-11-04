5cb7a888e56f7e7c23a4624e4005219deed6b032
Filip S. Perisic <pefi1011@gmail.com>
2019-11-04 21:21:22 +0100

    Implemented the programmatic routing for the save button

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const MovieForm = ({ match }) => {
+const handleSaveButton = history => {
+  history.replace("/movies");
+};
+
+const MovieForm = ({ match, history }) => {
   const { movieId } = match.params;
   return (
     <React.Fragment>
       <h2>Movie Form {movieId}</h2>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => handleSaveButton(history)}
+      >
+        Save
+      </button>
     </React.Fragment>
   );
 };
