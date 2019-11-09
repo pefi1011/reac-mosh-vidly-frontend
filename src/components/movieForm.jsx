73d91acf0a142f6928e4d98af6821cf854d7c4b4
Filip S. Perisic <pefi1011@gmail.com>
2019-11-09 17:30:00 +0100

    Added first version of the movieForm

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,23 +1,60 @@
 import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-const handleSaveButton = history => {
-  history.replace("/movies");
-};
+class MovieForm extends Form {
+  state = {
+    data: { title: "", genre: "", numberInStock: "", dailyRentalRate: "" },
+    errors: {}
+  };
 
-const MovieForm = ({ match, history }) => {
-  const { movieId } = match.params;
-  return (
-    <React.Fragment>
-      <h2>Movie Form {movieId}</h2>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => handleSaveButton(history)}
-      >
-        Save
-      </button>
-    </React.Fragment>
-  );
-};
+  // Define JOI SCHEMA
+  // it's not part of the state bc it does not have to change
+  schema = {
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genre: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .positive()
+      .less(6)
+      .integer()
+      .required()
+      .label("Stock"),
+    dailyRentalRate: Joi.number()
+      .greater(0)
+      .less(6)
+      .precision(2)
+      .required()
+      .label("Rate")
+  };
+
+  doSubmit = history => {
+    console.log("Form submitted");
+    // Save the changes
+    // Redirect the user
+
+    history.replace("/movies");
+  };
+
+  render() {
+    // In our render method we do the object destructuring
+
+    return (
+      <React.Fragment>
+        <h2>Movie Form {"movieId"}</h2>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => this.handleSaveButton("history")}
+        >
+          Save
+        </button>
+      </React.Fragment>
+    );
+  }
+}
 
 export default MovieForm;
