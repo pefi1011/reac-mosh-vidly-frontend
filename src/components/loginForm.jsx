f23f62aa5a0b4e5568fa2564f7ce3f25dfd0a193
Filip S. Perisic <pefi1011@gmail.com>
2019-11-09 12:02:46 +0100

    Preventing full page reload on form submit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 
 class LoginForm extends Component {
   state = {};
+
+  handleSubmit = e => {
+    // In this case, prevents submitting
+    // the form to the server which causes the full page reload
+    e.preventDefault();
+
+    // Call the server
+    console.log("Form submitted");
+    // Save the changes
+    // Redirect the user
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        {/** BY DEFAULT HTML FORM MAKE A FULL ROUND-TRIP TO THE SERVER
+        i.e. THE WHOLE PAGE GET RELOADED!!! THAT'S NOT WHAT WE WANT 
+        THEREFORE WE OVERRIDE THE onSubmit event!
+        */}
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
