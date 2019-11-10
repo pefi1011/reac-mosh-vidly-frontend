0a78686f5900d6080fbea98f7ad575f7d30c7b1a
Filip S. Perisic <pefi1011@gmail.com>
2019-11-10 20:10:20 +0100

    Replaced fakeGenreService with genreService

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,14 +1,16 @@
 import Raven from "raven-js";
 
 function init() {
-  Raven.config("https://7fb8306ffb0a43caa3cef3963d8b1bca@sentry.io/1814815", {
-    release: "0-0-1",
-    environment: "development-test"
-  }).install();
+  // Raven.config("https://7fb8306ffb0a43caa3cef3963d8b1bca@sentry.io/1814815", {
+  //   release: "0-0-1",
+  //   environment: "development-test"
+  // }).install();
 }
 
 function log(error) {
-  Raven.captureException(error);
+  // Raven.captureException(error);
+
+  console.error(error);
 }
 
 export default {
