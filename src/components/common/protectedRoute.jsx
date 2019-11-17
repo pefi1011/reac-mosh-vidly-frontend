fb44cd2008e8a4af89c5794eb0f1a260c5764683
Filip S. Perisic <pefi1011@gmail.com>
2019-11-17 16:32:16 +0100

    Made protectedRoute component

---
+++ src/components/common/protectedRoute.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import authService from "../../services/authService";
+
+// destructuring props object into path, component, and render
+// the props object can have more properties than path, component, render
+// so using the rest operator, we can pick other properties
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+  return (
+    <Route
+      path={path}
+      {...rest}
+      render={props => {
+        if (!authService.getCurrentUser()) return <Redirect to="login" />;
+
+        // If component is truthy, we return it
+        // otherwise, we return the render function
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    ></Route>
+  );
+};
+
+export default ProtectedRoute;
